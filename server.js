81f802c4939e8f09245ea8dd69e6feb8a8cfdf67
ESMAT001 <esmat10.niazi@gmail.com>
2021-07-21 23:05:41 +0430

    404 added for api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3001
 
 const server = express()
 server.use("/v1", mainRouter)
-// server.get("*", (req, res) => handle(req, res))
+server.get("*", (req, res) => res.status(404).send("resource not found!"))
 server.listen(port, err => {
     if (err) throw err;
     console.log("server and app running on port " + port)

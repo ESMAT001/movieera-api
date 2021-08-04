123e13de120ca47d35ecca8148defff92db5c2aa
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-04 23:41:30 +0430

    test push

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const apiRouter = require('./routers/api')
 const imageRouter = require('./routers/image')
 const router = express.Router();
 
-router.use('/', apiRouter)
 router.use('/image', imageRouter)
-
+router.use('/', apiRouter)
 router.get("*", (req, res) => res.status(404).send("404!"))
 
 module.exports = router

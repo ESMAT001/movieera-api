0c80b4ccd10d114608833288dd3779024d25964f
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-04 23:36:21 +0430

    test push

--- src/index.js
+++ src/index.js
@@ -3,28 +3,6 @@ const apiRouter = require('./routers/api')
 const imageRouter = require('./routers/image')
 const router = express.Router();
 
-const whiteList = [
-    'https://test.com/',
-    'http://test.com/',
-    "https://movieera.esmat@001.com",
-    'https://movieera-taupe.vercel.app/',
-    'https://rapidapi.com/'
-]
-const customMultipleCors = (whiteList) => {
-    return (req, res, next) => {
-        const origin = req.headers.origin;
-        console.log(req.headers)
-        if (whiteList.indexOf(origin) === -1) {
-            return res.status(403).send('Forbidden');
-        }
-        res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        // console.log(req.headers.origin) 
-        next()
-    }
-}
-
-router.use(customMultipleCors(whiteList))
 router.use('/', apiRouter)
 router.use('/image', imageRouter)
 
--- src/routers/api.js
+++ src/routers/api.js
@@ -28,6 +28,30 @@ const router = express.Router();
 
 router.use(express.json())
 
+const whiteList = [
+    'https://test.com/',
+    'http://test.com/',
+    "https://movieera.esmat@001.com",
+    'https://movieera-taupe.vercel.app/',
+    'https://rapidapi.com/'
+]
+const customMultipleCors = (whiteList) => {
+    return (req, res, next) => {
+        const origin = req.headers.origin;
+        console.log(req.headers)
+        if (whiteList.indexOf(origin) === -1) {
+            return res.status(403).send('Forbidden');
+        }
+        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        // console.log(req.headers.origin) 
+        next()
+    }
+}
+
+router.use(customMultipleCors(whiteList))
+
+
 
 router.get('/trending', async function (req, res) {
     const db = await connectToDb(dbName)

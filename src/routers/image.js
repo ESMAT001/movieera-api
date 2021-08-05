fd76d163d905a9c288aa4e61fe4306c4e15ba3b8
ESMAT001 <esmat10.niazi@gmail.com>
2021-08-05 12:38:36 +0430

    placeholder img route added

--- src/routers/image.js
+++ src/routers/image.js
@@ -4,6 +4,12 @@ const got = require('got')
 
 const router = express.Router()
 const url = 'https://image.tmdb.org/t/p/w500/'
+const placeholderImgUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/'
+
+router.get('/placeholder/:imgId', (req, res) => {
+    const { imgId } = req.params
+    res.redirect(placeholderImgUrl + imgId)
+})
 
 router.get('/:imgId', (req, res) => {
     const { imgId } = req.params

0d4157929c1155a741aaf6c42f60a2bffabedf62
ESMAT001 <esmat10.niazi@gmail.com>
2022-04-10 15:01:26 +0430

    search bug fixed

--- src/routers/functions/search.js
+++ src/routers/functions/search.js
@@ -21,16 +21,11 @@ async function search(db, query, limit, page, mode = "full") {
     const skip = (page - 1) * limit
     query = sanitize(query);
     let results = await db.collection('movie').find({
-        $and: [
-            {
-                $or: [
-                    { title: { $regex: query, $options: 'i' } },
-                    { original_title: { $regex: query, $options: 'i' } },
-                    { id: parseInt(query) },
-                    { imdb_id: query }
-                ]
-            },
-            { status: "Released" }
+        $or: [
+            { title: { $regex: query, $options: 'i' } },
+            { original_title: { $regex: query, $options: 'i' } },
+            { id: parseInt(query) },
+            { imdb_id: query }
         ]
     }, {
         projection

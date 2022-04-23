430310f6611c125d3da23ce884368b9f1b255761
ESMAT001 <esmat10.niazi@gmail.com>
2022-04-23 15:12:05 +0430

    placeholder added

---
+++ src/routers/functions/placeholderImage.js
@@ -0,0 +1,40 @@
+const got = require('got');
+const { placeholderImageGenratorUrl } = require('../../utils')
+
+const singleMoviePlaceholderImage = async (imagePath, type = "base64") => {
+    try {
+        const { base64 } = await got(`${placeholderImageGenratorUrl}?src=https://image.tmdb.org/t/p/w300_and_h450_bestv2/${imagePath[0] === "/" ? imagePath.slice(1) : imagePath}&type=${type}`).json();
+
+        return base64;
+    } catch (error) {
+        return null;
+    }
+}
+
+const multiMoviePlaceholderImage = async (movieData, type = "base64") => {
+    // console.log(movieData)
+    try {
+        return await Promise.all(movieData.map(async (movie) => ({
+            ...movie,
+            placeholder: await singleMoviePlaceholderImage(movie.poster_path)
+        }))).then(data => data);
+
+        // const res= []
+        // for (const movie of movieData) {
+        //     res.push({
+        //         ...movie,
+        //         placeholder: await singleMoviePlaceholderImage(movie.poster_path)
+        //     })
+        // }
+        // return res;
+
+    } catch (error) {
+        return movieData;
+    }
+}
+
+
+module.exports = {
+    singleMoviePlaceholderImage,
+    multiMoviePlaceholderImage
+}
